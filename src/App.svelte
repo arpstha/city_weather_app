9e6169cdda4ad3d3bad2ca0a084fa4cad50cebd4
arpstha <shrestharp88@gmail.com>
2024-04-19 14:40:12 +0100

    showing current weather with fetch

---
+++ src/App.svelte
@@ -0,0 +1,73 @@
+<script>
+	import { writable } from 'svelte/store';
+	import Weather from "./Weather.svelte";
+	import { onMount } from 'svelte';
+  
+	export const currentCity = writable('');
+	export const weatherData = writable(null);
+  
+	onMount(async () => {
+	  fetchWeather('london');
+	});
+  
+	async function fetchWeather(city) {
+	  try {
+		const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=db707a8803a4ca18f60ef2c268914634`);
+		
+		if (!response.ok) {
+		  throw new Error('Failed to fetch weather data');
+		}
+		const data = await response.json();
+		weatherData.set(data); // Update the weatherData store with the fetched data
+	  } catch (error) {
+		console.error(error);
+	  }
+	}
+  
+	function handlesubmit(event) {
+	  event.preventDefault();
+	  const inputValue = $currentCity;
+	  if (inputValue.trim() !== '') {
+		currentCity.set(inputValue);
+		fetchWeather($currentCity)
+	  }
+	}
+	
+	function clear(event){
+	  event.preventDefault();
+	  currentCity.set('');
+	  weatherData.set(null); // Clear the weatherData when clearing the city
+	}
+  </script>
+  
+  <main>
+	<form>
+	  <input placeholder='Search weather by city' bind:value={$currentCity}/>
+	  <button on:click={handlesubmit}>Find Weather</button>
+	  <button on:click={clear}>Clear</button>
+	</form>
+	{#if $weatherData} <!-- Check if weatherData is available -->
+	  <h1>The current weather in {$weatherData.name}:</h1>
+	  <Weather {weatherData} />
+	{:else}
+	  <p>No weather data available</p>
+	{/if}
+  </main>
+  
+  <style>
+	main {
+	  text-align: center;
+	  padding: 1em;
+	  max-width: 240px;
+	  margin: 0 auto;
+	}
+  
+	h1 {
+	  color: #ff3e00;
+	  text-transform: uppercase;
+	  font-size: 1.5em;
+	  font-weight: 100;
+	  margin-bottom: 0%;
+	}
+  </style>
+  
---
+++ src/Weather.svelte
@@ -0,0 +1,55 @@
+<script>
+  
+  export let weatherData
+ 
+  let imageUrl = "https://openweathermap.org/img/wn/{$weatherData.weather[0].icon}@2x.png";
+</script>
+
+<div class="container">
+  <div class="card">
+    <div class="card-header">
+      <h2>{$weatherData.name}, {$weatherData.sys.country}</h2>
+    </div>
+    <div class="card-body">
+      <div class="weather-container">
+        <img src={`https://openweathermap.org/img/wn/${$weatherData.weather[0].icon}@2x.png`} alt="Weather condition image">
+        <p>{Math.floor($weatherData.main.temp - 273.15)}°C</p>
+      </div>
+      <p>{$weatherData.weather[0].description}, feels like {Math.floor($weatherData.main.feels_like - 273.15)}°C</p>
+      <p>Humidity: {$weatherData.main.humidity}%</p>
+      <p>Wind Speed: {$weatherData.wind.speed} m/s</p>
+      <p>Visibility: {$weatherData.visibility} meters</p>
+    </div>
+  </div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  .card {
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    overflow: hidden;
+    width: 350px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+
+  .card-header {
+    background-color: #f0f0f0;
+    padding: 10px;
+    text-align: center;
+  }
+
+  .card-body {
+    padding: 20px;
+  }
+  .weather-container{
+    font-size: large;
+    font-weight: 600;
+  }
+</style>
